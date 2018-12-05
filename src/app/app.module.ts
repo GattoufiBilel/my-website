5f24ff44f08157e707a951a54c6fb0125b970b12
Haikel Fazzani <haikel.fazzani@gmail.com>
2018-12-05 13:03:11 +0100

    initial commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,14 +1,28 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { AppRoutingModule } from './routing/app-routing.module';
 
 import { AppComponent } from './app.component';
+import { NavbarComponent } from './partials/navbar/navbar.component';
+import { HomeComponent } from './components/home/home.component';
+import { ProjectsComponent } from './components/projects/projects.component';
+import { AboutComponent } from './components/about/about.component';
+import { BlogComponent } from './components/blog/blog.component';
+import { ContactComponent } from './components/contact/contact.component';
+
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    NavbarComponent,
+    HomeComponent,
+    ProjectsComponent,
+    AboutComponent,
+    BlogComponent,
+    ContactComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule , AppRoutingModule
   ],
   providers: [],
   bootstrap: [AppComponent]
---
+++ src/app/routing/app-routing.module.ts
@@ -0,0 +1,20 @@
+import { Routes, RouterModule } from '@angular/router';
+import { NgModule } from '@angular/core';
+import { HomeComponent } from '../components/home/home.component';
+import { ProjectsComponent } from '../components/projects/projects.component';
+import { BlogComponent } from '../components/blog/blog.component';
+import { AboutComponent } from '../components/about/about.component';
+
+const routes: Routes = [
+    { path: '', component: HomeComponent },
+    { path: 'projects', component: ProjectsComponent },
+    { path: 'blog', component: BlogComponent },
+    { path: 'about', component: AboutComponent },
+    { path: '**', component: HomeComponent }
+];
+
+@NgModule({
+    imports: [RouterModule.forRoot(routes)],
+    exports: [RouterModule]
+})
+export class AppRoutingModule {}
