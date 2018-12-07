132d5efe1835b26a19610a7969878c912fbab7af
Haikel Fazzani <haikel.fazzani@gmail.com>
2018-12-07 23:50:31 +0100

    update app

--- src/app/components/about/about.component.ts
+++ src/app/components/about/about.component.ts
@@ -9,7 +9,9 @@ export class AboutComponent implements OnInit {
 
   certificationLinks : Array<string> = [ 
     'https://mega.nz/#!COwSDC6A!3RneNG-7lGg666MoS0j5EhbbqRbeq7Vs1mTDoy_UZbE' ,
-    'https://mega.nz/#!HXoE1YgA!7AuR5EP5Mq27pUrROthIUZLMOl3wqJSnjebzds7OKvs' ]
+    'https://mega.nz/#!HXoE1YgA!7AuR5EP5Mq27pUrROthIUZLMOl3wqJSnjebzds7OKvs' ];
+
+    twitterLink : string = 'https://twitter.com/intent/follow?screen_name=HaikelFazzani&tw_p=followbutton&ref_src=twsrc%5Etfw';
 
   constructor() { }
 
