b8be3c94b605e354cc246dc8c54f4c8263f27fc3
Haikel Fazzani <haikel.fazzani@hotmail.com>
2018-12-26 12:47:12 +0100

    update app

--- src/app/components/about/about.component.ts
+++ src/app/components/about/about.component.ts
@@ -7,6 +7,15 @@ import { Component, OnInit } from '@angular/core';
 })
 export class AboutComponent implements OnInit {
 
+  links : Array<string> = [
+    "https://www.facebook.com/hai.kel.1829" ,
+    "https://twitter.com/HaikelFazzani" ,
+    "https://github.com/haikelfazzani" ,    
+    "" ,   
+    "https://gitlab.com/haikelfazzani" ,
+    "https://codepen.io/haikelfazzani-the-bold/" 
+  ];
+
   certificationLinks : Array<string> = [ 
     'https://mega.nz/#!COwSDC6A!3RneNG-7lGg666MoS0j5EhbbqRbeq7Vs1mTDoy_UZbE' ,
     'https://mega.nz/#!HXoE1YgA!7AuR5EP5Mq27pUrROthIUZLMOl3wqJSnjebzds7OKvs' ];
